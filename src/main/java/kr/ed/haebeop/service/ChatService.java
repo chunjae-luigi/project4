6c68355690932cd25b4cfe13f38b79ccf3b380a0
sangmin <sangmin990816@gmail.com>
2023-10-20 19:40:30 +0900

    채팅 기능적으로는 완료

--- src/main/java/kr/ed/haebeop/service/ChatService.java
+++ src/main/java/kr/ed/haebeop/service/ChatService.java
@@ -5,11 +5,7 @@ import kr.ed.haebeop.domain.ChatRoom;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.util.*;
 
 @Slf4j
@@ -44,13 +40,5 @@ public class ChatService {
 
         chatRooms.put(roomId, room); // 랜덤 아이디와 room 정보를 Map 에 저장
         return room;
-    }
-
-    public <T> void sendMessage(WebSocketSession session, T message) {
-        try{
-            session.sendMessage(new TextMessage(mapper.writeValueAsString(message)));
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        }
     }
 }
