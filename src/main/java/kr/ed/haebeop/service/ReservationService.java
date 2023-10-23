e6437e44b1bb99f99e5c9b3604d999a8abce5b92
SAM <sangmin990816@gmail.com>
2023-10-23 10:44:58 +0900

    Sangmin 내 예약 확인, properties 수정 (#23)
    * sangming
    
    * calendar
    
    * calendar
    
    * reservations
    
    * chat 도전
    
    * f chat
    
    * naver login
    
    * 채팅 기능적으로는 완료
    
    * 쿠키 설정
    
    * 기타
    
    * commit

--- src/main/java/kr/ed/haebeop/service/ReservationService.java
+++ src/main/java/kr/ed/haebeop/service/ReservationService.java
@@ -3,6 +3,7 @@ package kr.ed.haebeop.service;
 import kr.ed.haebeop.domain.Reservation;
 import kr.ed.haebeop.util.Page;
 
+import javax.servlet.ServletContext;
 import java.io.IOException;
 import java.util.List;
 
@@ -10,7 +11,8 @@ public interface ReservationService {
     public List<Reservation> reservationList(Page page);
     public List<Reservation> reservationGetTimeList(Reservation reservation);
     public Reservation reservationGet(int rno);
-    public boolean reservationInsert(Reservation reservation) throws IOException;
+    public boolean reservationInsert(Reservation reservation, ServletContext servletContext) throws IOException;
     public int reservationCount();
     public void reservationUpdateStatus(Reservation reservation);
+    public List<Reservation> reservationMyList(String id);
 }
