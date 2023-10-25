b3e23be0398e0d78bbd5297e709d691f2692fc29
SAM <sangmin990816@gmail.com>
2023-10-25 15:44:38 +0900

    Sangmin 유저간 채팅 수정 (#49)
    * sangming
    
    * calendar
    
    * calendar
    
    * reservations
    
    * chat 도전
    
    * f chat
    
    * naver login
    
    * 채팅 기능적으로는 완료
    
    * 쿠키 설정
    
    * 기타
    
    * commit
    
    * 인터셉터
    
    * admin interceptor
    
    * reservation list 수정
    
    * readme
    
    * readme
    
    * 유저끼리 채팅

--- src/main/java/kr/ed/haebeop/domain/UserChat.java
+++ src/main/java/kr/ed/haebeop/domain/UserChat.java
@@ -9,14 +9,14 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatDTO {
+public class UserChat {
     // 메시지  타입 : 입장, 채팅
     public enum MessageType{
         ENTER, TALK, LEAVE, NOTICE
     }
 
     private MessageType type; // 메시지 타입
-    private String roomId; // 방 번호
+    private String name;    // 방 이름
     private String sender; // 채팅을 보낸 사람
     private String message; // 메시지
     private String time; // 채팅 발송 시간
