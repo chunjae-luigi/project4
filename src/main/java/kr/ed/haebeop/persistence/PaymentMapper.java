25cc86eaa567d2e80c6c9397ac9f26bf0acff7c2
sendjin5 <sendjin5@gmail.com>
2023-10-25 14:26:08 +0900

    124214

--- src/main/java/kr/ed/haebeop/persistence/PaymentMapper.java
+++ src/main/java/kr/ed/haebeop/persistence/PaymentMapper.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Mapper
 public interface PaymentMapper {
 
-    public List<Payment> paymentList_Member(String id) throws Exception;
+    public int paymentList_Member(String id, int pno) throws Exception;
     public Payment paymentList_Lecture(int dno) throws Exception;
     public List<Payment> paymentList_admin(Page page) throws Exception;
     public List<Payment> paymentList_mypage(Page page) throws Exception;
