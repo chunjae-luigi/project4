4998c8bd50352b7fab6a154dfa6ac479321209a0
sangmin <sangmin990816@gmail.com>
2023-10-25 11:34:26 +0900

    유저끼리 채팅

--- src/main/java/kr/ed/haebeop/controller/UserSocketController.java
+++ src/main/java/kr/ed/haebeop/controller/UserSocketController.java
@@ -2,24 +2,23 @@ package kr.ed.haebeop.controller;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kr.ed.haebeop.domain.ChatDTO;
+import kr.ed.haebeop.domain.UserChat;
 import org.springframework.stereotype.Controller;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Controller
-@ServerEndpoint(value = "/socket")
-public class SocketController {
+@ServerEndpoint(value = "/usersocket")
+public class UserSocketController {
     private static final ObjectMapper mapper = new ObjectMapper();
     private static final List<Session> sessionList = new ArrayList<Session>();
 
-    public SocketController(){
+    public UserSocketController(){
     }
 
 
@@ -32,13 +31,13 @@ public class SocketController {
     public void onMessage (String message, Session session) throws IOException {
         // 다른 사람에게 메세지 보내기
         Map<String, List<String>> requetParameter = session.getRequestParameterMap();
-        String roomId = requetParameter.get("roomId").get(0);
+        String roomId = requetParameter.get("name").get(0);
 
-        ChatDTO chat = mapper.readValue(message, ChatDTO.class);
+        UserChat chat = mapper.readValue(message, UserChat.class);
 
         if(chat.getSender().equals("admin")){
             // 관리자는 공지인 경우에만 메시지 전송
-            if(chat.getType().equals(ChatDTO.MessageType.NOTICE)){
+            if(chat.getType().equals(UserChat.MessageType.NOTICE)){
                 sendAllSessionToMessage(message);
             }
         } else {
@@ -58,7 +57,7 @@ public class SocketController {
 
     private void sendAllSessionToMessage(String msg){ // 연결된 모든 사용자에게 메세지 전달
         try {
-            for(Session s : SocketController.sessionList){
+            for(Session s : UserSocketController.sessionList){
                 s.getBasicRemote().sendText(msg);
             }
         } catch (Exception e) {
@@ -68,9 +67,9 @@ public class SocketController {
 
     private void sendRoomMessage(String msg, String roomId){
         try {
-            for(Session s : SocketController.sessionList){
+            for(Session s : UserSocketController.sessionList){
                 Map<String, List<String>> requetParameter = s.getRequestParameterMap();
-                String sroomId = requetParameter.get("roomId").get(0);
+                String sroomId = requetParameter.get("name").get(0);
                 if(sroomId.equals(roomId)){
                     s.getBasicRemote().sendText(msg);
                 }
